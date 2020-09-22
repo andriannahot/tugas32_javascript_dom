3430c8a633dc554ad6e85b15de29014342919017
andriannahot <64577548+andriannahot@users.noreply.github.com>
2020-09-22 09:20:23 +0700

    Add files via upload

---
+++ tugas32.js
@@ -0,0 +1,9 @@
+function panggilEksternal(){
+	var nd = document.getElementById('namadepan')
+	var nb = document.getElementById('namabelakang')
+	var hb = document.getElementById('hobi')
+
+	console.log(nd)
+	console.log(nb)
+	console.log(hb)
+}
